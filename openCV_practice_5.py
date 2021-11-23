325cbcdabf969b8648a377c3993ce5074e5538b3
Midi Nanakul <midinanakul@hotmail.com>
2021-11-22 23:05:13 -0800

    Added file to gitignore. Created a script that detects the corners of a chessboard image.

---
+++ openCV_practice_5.py
@@ -0,0 +1,24 @@
+import numpy as np
+from cv2 import cv2
+
+# Create variable that holds our image
+img = cv2.imread('../Camera_Assets/ChessBoard.jpeg')
+
+# Convert image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Corner Detection == (src image, number of corners, minimum quality, minimum euclidean distance)
+corners = (cv2.goodFeaturesToTrack(gray, 100, 0.01, 10))
+# Cast to integer since goodFeaturesToTrack returns floating points
+corners = np.int0(corners)
+
+# Draw a circle around each corner
+for corner in corners:
+    # ravel flattens arrays
+    x, y = corner.ravel()
+    cv2.circle(img, (x, y), 5, (0, 0, 255), 1)
+
+
+cv2.imshow('Chess', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
